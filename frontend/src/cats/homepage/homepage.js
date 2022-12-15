bde07b8ef17d7d01c2a4e5fa711a3d8a4c3ca493
Caroline Hughes <carolinehughes1717@gmail.com>
2022-12-15 13:36:15 -0500

    homepage progress

--- frontend/src/cats/homepage/homepage.js
+++ frontend/src/cats/homepage/homepage.js
@@ -1,49 +1,71 @@
-const API_URL = `https://api.thecatapi.com/v1/images/search?limit=5`;
+import React, { useState, useEffect } from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import "./styles.css"
+
+const API_URL = `https://api.thecatapi.com/v1/images/search?limit=24&size=small`;
 const API_KEY = "live_i4AlnomPIvkmpQj0FlbDiUFBnxJX6SUNuP7VNcbH1B7s19Onxn3vYzynt285VM1k"
 
-fetch(API_URL,{headers: {
-    'x-api-key': API_KEY
-  }})
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    let imagesData = data;
-    <div className="image-grid col-12">
+function Homepage(props) {
 
-    </div>
-    imagesData.map(function(imageData) {
-      
-      let image = document.createElement('img');
-      image.src = `${imageData.url}`;
-    //   image.width(100);
-    //   image.height(100);
-          
-      let gridCell = document.createElement('div');
-      gridCell.classList.add('col');
-      gridCell.classList.add('col-lg');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setCurrentUser] = useState(
+      {
+        username: "catluver88",
+        favbreeds: []
+      }
+    );
 
-      gridCell.appendChild(image)
-        
-      document.getElementById('grid').appendChild(gridCell);
+  // if !loggedIn, random cats
+  // if loggedIn, cats of ur fav_breeds
+  const [mainPanelData, setMainPanelData] = useState([]);
 
-      
-      });
-  })
+  // if !loggedIn, activityList is list of n most recent ppl who have joined (registered)
+  // if loggedIn, activityList is most recent activity of you and your followers
+  const [activityList, setActivityList] = useState([]); 
 
-function Homepage() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const catsData = await fetch(API_URL,
+        {headers: {
+        'x-api-key': API_KEY
+        }})
+      const json = await catsData.json();
+      setMainPanelData(json);
+    }
+  
+    fetchData()
+    .catch(console.error);
+    }, [])
+
     return(
         <div className="container">
             <div className="header row mt-3">
-                <h2 className="title col-10">Cats App</h2>
+                <h2 className="title col-10">Cats FYP</h2>
             </div>
             <div className="panels row">
-                <div className="main-panel col-8">
-                    <p>cats pulled from API</p>
-                    <div id="grid" class="imgrid"></div>
+                <div className="col-9">
+                    {loggedIn ? 
+                      <p>Hey @{user.username}! Here are some cats of your favorite breeds:</p>
+                      : <p>Hey stranger! Log in or register to see your customized cats homepage</p>
+                    }
+                    <ImageList cols={3}>
+                    {mainPanelData.map((pic) => (
+                      <ImageListItem key={pic.id}>
+                        <img
+                          alt="a cat" 
+                          src={pic.url}
+                          loading="lazy"
+                          width={400}
+                        />
+                      </ImageListItem>
+                    ))}
+                  </ImageList>
+
                 </div>
-                <div className="right-panel col-4">
-                    <p>activity list</p>
+                <div className="right-panel col-md">
+                    <p>Activity List:</p>
                 </div>
             </div>
         </div>
