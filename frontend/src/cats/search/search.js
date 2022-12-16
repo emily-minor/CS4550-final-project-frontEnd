fb4667b501ddbfa0ce585a17266f1e00c0f50695
emilymlynn <minor.e@husky.neu.edu>
2022-12-16 05:22:35 -0500

    finally have search bar actually clicking

--- frontend/src/cats/search/search.js
+++ frontend/src/cats/search/search.js
@@ -1,10 +1,46 @@
-function Search() {
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://www.google.com/search?q=site%3A+';
+const site = 'pagedart.com';
+
+// NOTE: this code is based on this code from:
+// https://pagedart.com/blog/how-to-add-a-search-bar-in-html/
+// to help create a search bar
+
+function submitted() {
+    // event.preventDefault();
+    console.log("hello")
+    // const url = google + site + '+' + q.value;
+    // const win = window.open(url, '_blank');
+    // win.focus();
+}
+
+function searchButton() {
+    var button = document.createElement('BUTTON');
+    button.addEventListener("click", submitted());
     return(
-        <div>
-            <h2>Search</h2>
-            <input type="search" id="query" name="catSearch" placeholder="Search up some cats :)"/>
-            <button>Search</button>
-        </div>
+        <button>
+            Search
+        </button>
     )
 }
-export default Search
+
+function searchBar() {
+    return(
+        <form id="form" role="search">
+            <link rel="stylesheet" href="./searchBar.css"/>
+            <h2>Search</h2>
+            <input type="search" id="query" name="" placeholder="Search up some cats :)"/>
+            {searchButton()}
+        </form>
+
+    );
+}
+
+function Search() {
+    return(
+        searchBar()
+    );
+
+}export default Search
+
