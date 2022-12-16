4d576178b65df5b5b731c8e82568941fc13dbfa7
emilymlynn <minor.e@husky.neu.edu>
2022-12-16 04:14:54 -0500

    html for search bar

--- frontend/src/cats/search/search.js
+++ frontend/src/cats/search/search.js
@@ -2,6 +2,8 @@ function Search() {
     return(
         <div>
             <h2>Search</h2>
+            <input type="search" id="query" name="catSearch" placeholder="Search up some cats :)"/>
+            <button>Search</button>
         </div>
     )
 }
